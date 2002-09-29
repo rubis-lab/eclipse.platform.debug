dfc5c187949e23db351ebb900d20be78982b5671
Jared Burns <jburns>
2002-09-29 01:01:51 +0000

    AntAction works (does something useful)

--- org.eclipse.ui.externaltools/External Tools Base/org/eclipse/ui/externaltools/internal/ui/AntAction.java
+++ org.eclipse.ui.externaltools/External Tools Base/org/eclipse/ui/externaltools/internal/ui/AntAction.java
@@ -9,25 +9,25 @@ http://www.eclipse.org/legal/cpl-v05.html
  
 Contributors:
 **********************************************************************/
-import org.eclipse.ant.core.TargetInfo;
 import org.eclipse.core.resources.IFile;
-import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.MultiStatus;
 import org.eclipse.core.runtime.NullProgressMonitor;
 import org.eclipse.jface.action.Action;
-import org.eclipse.jface.dialogs.ErrorDialog;
 import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.jface.viewers.ILabelProvider;
 import org.eclipse.jface.wizard.WizardDialog;
+import org.eclipse.swt.custom.BusyIndicator;
 import org.eclipse.swt.widgets.Shell;
 import org.eclipse.ui.IWorkbenchWindow;
 import org.eclipse.ui.PlatformUI;
-import org.eclipse.ui.actions.NewWizardAction;
+import org.eclipse.ui.dialogs.ElementListSelectionDialog;
 import org.eclipse.ui.externaltools.internal.ant.dialog.AntExternalToolNewWizard;
-import org.eclipse.ui.externaltools.internal.ant.model.AntUtil;
 import org.eclipse.ui.externaltools.internal.model.DefaultRunnerContext;
+import org.eclipse.ui.externaltools.internal.model.ExternalToolsPlugin;
 import org.eclipse.ui.externaltools.internal.model.IHelpContextIds;
-import org.eclipse.ui.externaltools.internal.model.ToolMessages;
+import org.eclipse.ui.externaltools.internal.registry.ExternalToolRegistry;
+import org.eclipse.ui.externaltools.internal.view.ExternalToolLabelProvider;
 import org.eclipse.ui.externaltools.model.ExternalTool;
 import org.eclipse.ui.externaltools.model.ExternalToolStorage;
 import org.eclipse.ui.externaltools.model.IExternalToolConstants;
@@ -55,6 +55,62 @@ public class AntAction extends Action {
 		setToolTipText(file.getFullPath().toOSString());
 		WorkbenchHelp.setHelp(this, IHelpContextIds.ANT_ACTION);
 	}
+	
+	private ExternalTool findTool() {
+		ExternalToolRegistry registry= ExternalToolsPlugin.getDefault().getToolRegistry(getShell());
+		ExternalTool[] tools= registry.getToolsOfType(IExternalToolConstants.TOOL_TYPE_ANT_BUILD);
+		if (tools.length == 1) {
+			return tools[0];
+		} else if (tools.length > 1) {
+			return chooseTool(tools);
+		}
+		return null;
+	}
+	
+	private Shell getShell() {
+		return PlatformUI.getWorkbench().getActiveWorkbenchWindow().getShell();
+	}
+	
+	private ExternalTool chooseTool(ExternalTool[] tools) {
+		ILabelProvider labelProvider = new ExternalToolLabelProvider();
+		ElementListSelectionDialog dialog= new ElementListSelectionDialog(getShell(), labelProvider);
+		dialog.setElements(tools);
+		dialog.setTitle("Ant Tool Selection");
+		dialog.setMessage("Choose an ant tool to run");
+		dialog.setMultipleSelection(false);
+		int result = dialog.open();
+		labelProvider.dispose();
+		if (result == ElementListSelectionDialog.OK) {
+			return (ExternalTool) dialog.getFirstResult();
+		}
+		return null;		
+	}
+	
+	public void runTool(final ExternalTool tool) {
+		final MultiStatus status = new MultiStatus(IExternalToolConstants.PLUGIN_ID, 0, "", null);
+		BusyIndicator.showWhile(getShell().getDisplay(), new Runnable() {
+			public void run() {
+				new DefaultRunnerContext(tool, file).run(new NullProgressMonitor(), status);
+			}
+		});
+		if (!status.isOK()) {
+			StringBuffer message= new StringBuffer("An exception occurred while running ant: ");
+			IStatus[] errors= status.getChildren();
+			IStatus error;
+			for (int i= 0, numErrors= errors.length; i < numErrors; i++) {
+				error= errors[i];
+				if (error.getSeverity() == IStatus.ERROR) {
+					Throwable exception= error.getException();
+					message.append('\n');
+					if (exception != null) {
+						message.append(exception.getClass().getName()).append(' ');
+					}
+					message.append(error.getMessage());
+				}
+			}
+			MessageDialog.openError(PlatformUI.getWorkbench().getActiveWorkbenchWindow().getShell(), "Ant Error", message.toString());
+		}
+	}
 
 	/* (non-Javadoc)
 	 * Method declared on IAction.
@@ -64,34 +120,25 @@ public class AntAction extends Action {
 			return;
 		}
 		
-		new WizardDialog(PlatformUI.getWorkbench().getActiveWorkbenchWindow().getShell(), new AntExternalToolNewWizard(file)).open();
+		ExternalTool tool= findTool();
+		
+		AntExternalToolNewWizard wizard;
+		if (tool != null) {
+			runTool(tool);
+			return;
+		}
+		WizardDialog dialog= new WizardDialog(PlatformUI.getWorkbench().getActiveWorkbenchWindow().getShell(), new AntExternalToolNewWizard(file));
+		dialog.open();
+		if (dialog.getReturnCode() == WizardDialog.CANCEL) {
+			return;
+		}
 		ExternalToolStorage.addStorageListener(new IStorageListener() {
 			public void toolDeleted(ExternalTool tool) {
 			}
 
 			public void toolCreated(ExternalTool tool) {
-				if (tool.getLocation().equals(file.getLocation().toString())) {
-					MultiStatus status = new MultiStatus(IExternalToolConstants.PLUGIN_ID, 0, "", null);
-					new DefaultRunnerContext(tool, file).run(new NullProgressMonitor(), status);
-					Shell shell= PlatformUI.getWorkbench().getActiveWorkbenchWindow().getShell();
-					if (!status.isOK()) {
-						StringBuffer message= new StringBuffer("An exception occurred while running ant: ");
-						IStatus[] errors= status.getChildren();
-						IStatus error;
-						for (int i= 0, numErrors= errors.length; i < numErrors; i++) {
-							error= errors[i];
-							if (error.getSeverity() == IStatus.ERROR) {
-								Throwable exception= error.getException();
-								message.append('\n');
-								if (exception != null) {
-									message.append(exception.getClass().getName()).append(' ');
-								}
-								message.append(error.getMessage());
-							}
-						}
-						MessageDialog.openError(shell, "Ant Error", message.toString());
-					}
-					ExternalToolStorage.deleteTool(tool, shell);
+				if (tool.getLocation().equals(file.getLocation().toString())) { // Is this the tool we're expecting?
+					runTool(tool);
 				}
 				ExternalToolStorage.removeStorageListener(this);
 			}
@@ -102,32 +149,5 @@ public class AntAction extends Action {
 			public void toolsRefreshed() {
 			}
 		});
-		
-//		TargetInfo[] targetList = null;
-//		try {
-//			targetList = AntUtil.getTargets(file.getFullPath().makeAbsolute().toString()); // getTargetList(file.getLocation());
-//		} catch (CoreException e) {
-//			ErrorDialog.openError(
-//				window.getShell(),
-//				ToolMessages.getString("AntAction.runErrorTitle"), //$NON-NLS-1$
-//				ToolMessages.format("AntAction.errorReadAntFile", new Object[] {file.getFullPath().toString()}), //$NON-NLS-1$;
-//				e.getStatus());
-//			return;
-//		}
-//		
-//		if (targetList == null) {
-//			MessageDialog.openError(
-//				window.getShell(),
-//				ToolMessages.getString("AntAction.runErrorTitle"), //$NON-NLS-1$;
-//				ToolMessages.format("AntAction.noAntTargets", new Object[] {file.getFullPath().toString()})); //$NON-NLS-1$;
-//			return;
-//		}
-//
-//		AntLaunchWizard wizard = new AntLaunchWizard(targetList, file, window);
-//		wizard.setNeedsProgressMonitor(true);
-//		WizardDialog dialog = new WizardDialog(window.getShell(), wizard);
-//		dialog.create();
-//		WorkbenchHelp.setHelp(dialog.getShell(), IHelpContextIds.ANT_LAUNCH_WIZARD);		
-//		dialog.open();
 	}
 }
