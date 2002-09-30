0bdda50d91bd21cc7aa72e36161a748ba8af1aec
Darin Swanson <darins>
2002-09-30 20:16:19 +0000

    Bug 23132 - logfile option for ant doesnt seem to work

--- org.eclipse.ui.externaltools/Ant Runner Support/org/eclipse/ui/externaltools/internal/ant/logger/AntBuildLogger.java
+++ org.eclipse.ui.externaltools/Ant Runner Support/org/eclipse/ui/externaltools/internal/ant/logger/AntBuildLogger.java
@@ -1,68 +1,39 @@
 package org.eclipse.ui.externaltools.internal.ant.logger;
 
 /**********************************************************************
-Copyright (c) 2002 IBM Corp. and others.
-All rights reserved.   This program and the accompanying materials
-are made available under the terms of the Common Public License v0.5
+Copyright (c) 2000, 2002 IBM Corp.  All rights reserved.
+This file is made available under the terms of the Common Public License v1.0
 which accompanies this distribution, and is available at
-http://www.eclipse.org/legal/cpl-v05.html
- 
-Contributors:
+http://www.eclipse.org/legal/cpl-v10.html
 **********************************************************************/
-import java.io.PrintStream;
 
 import org.apache.tools.ant.BuildEvent;
-import org.apache.tools.ant.BuildLogger;
-import org.apache.tools.ant.Project;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.NullProgressMonitor;
 import org.eclipse.swt.widgets.Display;
-import org.eclipse.ui.externaltools.internal.model.ToolMessages;
 import org.eclipse.ui.externaltools.internal.ui.LogConsoleDocument;
 import org.eclipse.ui.externaltools.internal.ui.OutputStructureElement;
 
-public class AntBuildLogger implements BuildLogger {
+public class AntBuildLogger extends NullBuildLogger {
 
-	protected int priorityFilter = LogConsoleDocument.MSG_INFO;
-	private int logLength = 0;
-	private int lastTargetEndIndex = 0;
-
+	private int fLogLength = 0;
+	private int fLastTargetEndIndex = 0;
+	
 	public AntBuildLogger() {
-	}
-
-	/**
-	 * @see BuildListener#buildStarted(BuildEvent)
-	 */
-	public void buildStarted(BuildEvent event) {
-	}
-
-	/**
-	 * @see BuildListener#buildFinished(BuildEvent)
-	 */
-	public void buildFinished(BuildEvent event) {
-		handleException(event);
-	}
-
-	protected void handleException(BuildEvent event) {
-		Throwable exception = event.getException();
-		if (exception == null)
-			return;
-		logMessage(
-			ToolMessages.format(
-				"AntBuildLogger.buildException", // $NON-NLS-1$
-				new String[] { exception.toString()}),
-			LogConsoleDocument.MSG_ERR);	
 	}
 
 	/**
 	 * @see BuildListener#targetStarted(BuildEvent)
 	 */
 	public void targetStarted(BuildEvent event) {
-		createNewOutputStructureElement(event.getTarget().getName(), logLength);
+		createNewOutputStructureElement(event.getTarget().getName(), fLogLength);
 	}
 
 	protected void refreshConsoleTrees() {
 		final LogConsoleDocument doc = LogConsoleDocument.getInstance();
-		if (!doc.hasViews())
+		if (!doc.hasViews()) {
 			return;
+		}
 		// we get the display from the console #0 (that exists for sure because consoles!=null)
 		Display display = doc.getDisplay();
 		// create a new thread for synchronizing all the refresh operations
@@ -76,10 +47,7 @@ public class AntBuildLogger implements BuildLogger {
 	protected void createNewOutputStructureElement(String name, int index) {
 		LogConsoleDocument doc = LogConsoleDocument.getInstance();
 		OutputStructureElement newElement =
-			new OutputStructureElement(
-				name,
-				doc.getCurrentOutputStructureElement(),
-				index);
+			new OutputStructureElement(name, doc.getCurrentOutputStructureElement(), index);
 		doc.setCurrentOutputStructureElement(newElement);
 	}
 
@@ -90,7 +58,7 @@ public class AntBuildLogger implements BuildLogger {
 		handleException(event);
 		finishCurrentOutputStructureElement();
 		// store the end index of this target's log (so that we can use it later)
-		lastTargetEndIndex = logLength;
+		fLastTargetEndIndex = fLogLength;
 		refreshConsoleTrees();
 	}
 
@@ -110,66 +78,34 @@ public class AntBuildLogger implements BuildLogger {
 		refreshConsoleTrees();
 	}
 
-	/**
-	 * @see BuildListener#messageLogged(BuildEvent)
-	 */
-	public void messageLogged(BuildEvent event) {
-		logMessage(event.getMessage(), toConsolePriority(event.getPriority()));
-	}
-	
-	/**
-	 * Converts a Ant project's priority level to a priority
-	 * level used by the Log Console.
-	 */
-	private int toConsolePriority(int antPriority) {
-		switch (antPriority) {
-			case Project.MSG_ERR:
-				return LogConsoleDocument.MSG_ERR;
-			case Project.MSG_WARN:
-				return LogConsoleDocument.MSG_WARN;	
-			case Project.MSG_INFO:
-				return LogConsoleDocument.MSG_INFO;	
-			case Project.MSG_VERBOSE:
-				return LogConsoleDocument.MSG_VERBOSE;	
-			case Project.MSG_DEBUG:
-				return LogConsoleDocument.MSG_DEBUG;	
-			default:
-				return LogConsoleDocument.MSG_INFO;	
-		}
-	}
-
 	protected void logMessage(String message, int priority) {
-		if (priority > priorityFilter)
+		if (priority > getMessageOutputLevel()) {
 			return;
+		}
+		super.logMessage(message, priority);
 		message += '\n';
 		LogConsoleDocument doc = LogConsoleDocument.getInstance();
 		doc.append(message, priority);
-		logLength += message.length();
+		fLogLength += message.length();
 	}
 
 	protected void finishCurrentOutputStructureElement() {
 		LogConsoleDocument doc = LogConsoleDocument.getInstance();
 		// sets the index that indicates the end of the log part linked to this element
 		OutputStructureElement output = doc.getCurrentOutputStructureElement();
-		output.setEndIndex(logLength);
+		output.setEndIndex(fLogLength);
 		// and sets the current element to the parent of the element
 		doc.setCurrentOutputStructureElement(output.getParent());
 	}
 
 	protected void createNewOutputStructureElement(String name) {
-		createNewOutputStructureElement(name, logLength);
+		createNewOutputStructureElement(name, fLogLength);
 	}
-
-	public void setMessageOutputLevel(int level) {
-		this.priorityFilter = toConsolePriority(level);
-	}
-
-	public void setEmacsMode(boolean emacsMode) {
-	}
-
-	public void setErrorPrintStream(PrintStream err) {
-	}
-
-	public void setOutputPrintStream(PrintStream output) {
+	
+	private IProgressMonitor monitorFor(IProgressMonitor monitor) {
+		if (monitor == null) {
+			return new NullProgressMonitor();
+		}
+		return monitor;
 	}
 }
